e2a248993992cad8c794d8dbd19bfde76c68237a
levan0106 <levan0106@gmail.com>
2019-09-28 13:55:36 +0700

    Update code for makersheet

--- bwc-service/BWC.Model/MakerSheet.cs
+++ bwc-service/BWC.Model/MakerSheet.cs
@@ -16,5 +16,26 @@ namespace BWC.Model
         public int Quantity { get; set; }
         public string ColorName { get; set; }
         public string HoodWidth { get; set; }
+
+        public string LocationName { get; set; }
+        public string MaterialColorName { get; set; }
+        public string BoxColorName { get; set; }
+        public string BarColorName { get; set; }
+        public string GuideColorName { get; set; }
+        public string ControlColorName { get; set; }
+
+        public int? ControlSideId { get; set; }
+        public string ControlSideName { get; set; }
+        public int? RollId { get; set; }
+        public string RollName { get; set; }
+        public int? TSplineId { get; set; }
+        public string TSplineName { get; set; }
+        public int? BSplineId { get; set; }
+        public string BSplineName { get; set; }
+        public int? FlapId { get; set; }
+        public string FlapName { get; set; }
+        public string ControlHBOL { get; set; }
+        public string TubeDia { get; set; }
+        public string Notes { get; set; }
     }
 }
